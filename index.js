36926ee46de7dba37c41e1f0b783909db9fb863a
Aditi Deshpande <aditideshpande2908@gmail.com>
2023-04-01 18:25:03 +0530

    Add project design & code

---
+++ index.js
@@ -0,0 +1,45 @@
+const myEmojis = ["👨‍💻", "🍜", "📚"]
+const emojiContainerEle = document.getElementById("emojiContainer");
+const emojiInput = document.getElementById("emoji-input");
+const pushBtn = document.getElementById("push-btn");
+const unshiftBtn = document.getElementById("unshift-btn");
+const popBtn = document.getElementById("pop-btn");
+const shiftBtn = document.getElementById("shift-btn");
+
+
+function renderEmojis() {
+    emojiContainerEle.innerHTML = "";
+    for(let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span');
+        emoji.textContent = myEmojis[i];
+        emojiContainerEle.append(emoji);
+    }
+}
+
+renderEmojis();
+
+pushBtn.addEventListener('click',function() {
+    if(emojiInput.value) {
+        myEmojis.push(emojiInput.value);
+        emojiInput.value = "";
+    }
+    renderEmojis();
+})
+
+unshiftBtn.addEventListener('click',function() {
+    if(emojiInput.value) {
+        myEmojis.unshift(emojiInput.value);
+        emojiInput.value = "";
+    }
+    renderEmojis();
+})
+
+popBtn.addEventListener('click',function() {
+    myEmojis.pop();
+    renderEmojis();
+})
+
+shiftBtn.addEventListener('click',function() {
+    myEmojis.shift();
+    renderEmojis();
+})
